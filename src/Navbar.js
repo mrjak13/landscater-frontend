74437e3ede084b6102191c41ba50b4734817e2af
Justin Kenney <justinkenney13@gmail.com>
2019-08-12 14:52:22 -0400

    create navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+	return (
+		<div className="navbar">
+			<h4 className="nav-link">Home</h4>
+			<h4 className="nav-link">About</h4>
+			<h4 className="nav-link">Contact</h4>
+		</div>
+	)
+}
+
+export default Navbar;
