c8644d7bb2d7f63d07f3849390147462e69d4a98
Justin Kenney <justinkenney13@gmail.com>
2019-08-12 14:53:08 -0400

    imported navbar to app

--- src/App.js
+++ src/App.js
@@ -2,30 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './Welcome'
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div>
       <Welcome />
+      <Navbar />
     </div>
   );
 }
 
 export default App;
-    // <div className="App">
-
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
