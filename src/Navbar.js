7cbc36138723ffa989aade243dc8d5e521f40bd0
Justin Kenney <justinkenney13@gmail.com>
2019-08-13 11:46:03 -0400

    changed temp headers to links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
 	return (
 		<div className="navbar">
-			<h4 className="nav-link">Home</h4>
-			<h4 className="nav-link">About</h4>
-			<h4 className="nav-link">Contact</h4>
+			<h4 className="nav-link"><Link to="/home">Home</Link></h4>
+			<h4 className="nav-link"><Link to="/about">About</Link></h4>
+			<h4 className="nav-link"><Link to="/contact">Contact</Link></h4>
 		</div>
 	)
 }
