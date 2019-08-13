ef671df96419fe3432cf9bb061efde700fa46449
Justin Kenney <justinkenney13@gmail.com>
2019-08-13 11:44:57 -0400

    create contact page

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+	return(
+		<div className="contact-page">
+			<h1>Contact info</h1>
+			<p>Here will be email phone number and stuff</p>
+		</div>
+	)
+}
+
+export default Contact
