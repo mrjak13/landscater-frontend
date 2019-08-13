b31f9bc875d8a0c004e1b8cb10c3f5a06e80e030
Justin Kenney <justinkenney13@gmail.com>
2019-08-13 11:44:44 -0400

    create about page

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+	return(
+		<div>
+			<h1>About</h1>
+			<p>Hey this is where I am going to put all that jazz about you.</p>
+		</div>
+	)
+}
+
+export default About 
