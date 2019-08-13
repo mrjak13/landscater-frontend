0115d754faf7725acbb84b9dd0236a0d86f3e079
Justin Kenney <justinkenney13@gmail.com>
2019-08-13 11:46:54 -0400

    add switch and routes to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Logo from './Logo'
 import Welcome from './Welcome'
+import About from './About'
+import Contact from './Contact'
 import Navbar from './Navbar'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Welcome />
+    	<Logo />
       <Navbar />
+      <Switch>
+      	<Route exact path='/home' component={Welcome} />
+      	<Route exact path='/about' component={About} />
+      	<Route exact path='/contact' component={Contact} />
+      </Switch>
     </div>
   );
 }
